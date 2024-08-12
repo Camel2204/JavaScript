fa6047154561a9889c78873f178e8f88ca116c1b
Camel2204 <Ol78ie@gmail.com>
2024-08-12 17:48:50 +0300

    changed Task3

--- Task3/main.js
+++ Task3/main.js
@@ -58,13 +58,86 @@ while (box<20){
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
 let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-// let list = 0;
-// while (list<listOfItems.length){
-//     document.write(`<ul><li>${list}</li></ul>`);
-//     list++;
-// }
+for (let j = 0; j < listOfItems.length; j++) {
+    const listOfItem = listOfItems[j];
+    document.write(`<div><ul><li>${listOfItem}</li></ul></div>`);
+};
 
-console.log(listOfItems[0])
+//6 Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
 
+let products = [
+    {
+        title: 'Milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'Juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'Tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'Tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    }
+];
+for (const product of products) {
+    document.write(`
+                  <div class="product-card">
+                     <h3 class="product-title">${product.title} Price - ${product.price} UAH</h3> 
+                     <img src = "${product.image}" alt = "${product.title}">
+                    </div>`);
+                     
+}
+//7 за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
 
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (const user of users) {
+    if (user.status === true){
+        console.log(user.name, user.status);
+    }
+}
+console.log('')
 
+for (const user of users) {
+    if (user.age > 30){
+        console.log(user.name, user.age);
+    }
+}
+
+console.log('')
+
+for (const user of users) {
+    if (user.status === false){
+        console.log(user.name, user.status);
+    }
+}
